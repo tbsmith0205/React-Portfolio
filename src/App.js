ea80b35e9ab706072774ddc25ac4d48725487c22
Taylor Smith <tbsmith0205@gmail.com>
2021-07-05 23:17:29 -0600

    added assets folder with pic, resume, and style.css

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+import Header from "./components/header";
+import Main from "./components/Main";
+import Footer from "./components/footer";
+import "./components/style.css";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
